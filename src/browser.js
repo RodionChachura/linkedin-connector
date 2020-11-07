94fff6cd081b06d7ef5f996bc457ea5129c8cccd
Rodion Chachura <geekrodion@gmail.com>
2020-11-07 17:54:38 +0400

    process: opening outlook and press the button

--- src/browser.js
+++ src/browser.js
@@ -1,14 +1,33 @@
+const puppeteer = require('puppeteer')
+
+const OUTLOOK_PAGE = 'https://outlook.live.com/people/0'
+const NEW_CONTACT_TEXT = 'New contact'
+
+const getElementWithText = async (parent, tag, text) => {
+  const [result] = await parent.$x(`//${tag}[contains(., '${text}')]`)
+  return result
+}
+
 class Browser {
   constructor() {
     console.log('Browser:constructor()')
   }
 
-  openOutlook() {
-    console.log('Browser:openOutlook()')
+  async openOutlook() {
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: 'ws://127.0.0.1:9222/devtools/browser/05110622-7642-4fdc-9ec8-b4900e109b2d',
+      defaultViewport: null
+    })
+
+    this.outlookPage = await browser.newPage()
+    await this.outlookPage.goto(OUTLOOK_PAGE)
+    await this.outlookPage.waitForXPath(`//div[contains(., '${NEW_CONTACT_TEXT}') and @aria-disabled="false"]`)
   }
 
-  createContact() {
-
+  async createContact(email) {
+    console.log(`Createing contact with email: ${email}`)
+    const [newContactButton] = await this.outlookPage.$x(`//button[contains(., '${NEW_CONTACT_TEXT}')]`)
+    await newContactButton.click()
   }
 
   openLinkedInSection() {
--- src/business.js
+++ src/business.js
@@ -3,11 +3,12 @@ const { getModule } = require('awsdynamoutils')
 const { paginationAware, projectionExpression } = getModule()
 const scan = paginationAware('scan')
 const USERS_TABLE_NAME = 'pomodoro_users'
+const NEEDED_USER_FIELDS = ['email', 'name', 'sets', 'registrationDate']
 
 const getNewUsers = (lastDate) => 
   scan({
     TableName: USERS_TABLE_NAME,
-    ...projectionExpression(['email', 'name', 'sets']),
+    ...projectionExpression(NEEDED_USER_FIELDS),
     FilterExpression: 'registrationDate > :lastDate',
     ExpressionAttributeValues: {
       ':lastDate': lastDate
@@ -24,7 +25,15 @@ class Business {
     const state = this.storage.get()
     const users = await getNewUsers(state.lastDate)
     console.log(`New users count: ${users.length}`)
+    const orderedUsers = users
+      .sort((a, b) => a.registrationDate - b.registrationDate)
+      // for testing
+      .slice(0, 1)
     await this.browser.openOutlook()
+
+    for await (const user of orderedUsers) {
+      await this.browser.createContact(user.email)
+    }
   }
 }
 
